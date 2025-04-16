b13cb4f7feb25ad46425e18c86442565ac11bbaa
Renato L Batista <sonomel00@yahoo.com.br>
2025-04-16 07:51:25 -0300

    instala e configura os ícones fontawesome
    Existem algumas formas de se usar os ícones e confesso que achei meio
    chato de usar e por isso vou criar outro (ou outros) commits com as
    formas que encontrei.
    
    Para esse commit, estou utilizando uma forma que importa toda a família
    de ícones de brand para poder chamar o ícone do github. Pra isso eu
    faço a importação do prefixo 'fab' do pacote de ícones de brand e
    também a função library do core para poder adicionar o prefixo nessa
    library e chamar o ícone como string.
    
    Essa chamada por string possui um problema conhecido pela fontawesome e
    a solução atual é um contorno até que seja implementada uma solução
    definitiva e pode ser conferida no link abaixo:
    https://docs.fontawesome.com/web/use-with/react/add-icons#typescript-and-custom-icons-issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+library.add(fab)
+
 export function App() {
-  return <h1>Olá Mundo</h1>
+  // @ts-expect-error problema conhecido pela fontawesome e esta é a solução temporária indicada no próprio site https://docs.fontawesome.com/web/use-with/react/add-icons#typescript-and-custom-icons-issue
+  const gitHubIcon: IconProp = "fa-brands fa-github"
+  return <h1><FontAwesomeIcon icon={gitHubIcon} /> Olá Mundo</h1>
 }
