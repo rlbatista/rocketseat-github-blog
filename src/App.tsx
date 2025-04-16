5bf4d31a4c7bff229a0b89c072b814dddfe8f09c
Renato L Batista <sonomel00@yahoo.com.br>
2025-04-16 08:28:09 -0300

    adiciona exemplo de como usar ícone do fontawesome
    Esse outro exemplo importa o ícone do pacote apropriado e repassa ao
    objeto <FontAwesomeIcon />. O código fica menor e mais conciso porém
    tem a desvantagem de requerer a importação de cada ícone que será
    utilizado.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-library.add(fab)
-
 export function App() {
-  // @ts-expect-error problema conhecido pela fontawesome e esta é a solução temporária indicada no próprio site https://docs.fontawesome.com/web/use-with/react/add-icons#typescript-and-custom-icons-issue
-  const gitHubIcon: IconProp = "fa-brands fa-github"
-  return <h1><FontAwesomeIcon icon={gitHubIcon} /> Olá Mundo</h1>
+  return <h1><FontAwesomeIcon icon={faGithub} /> Olá Mundo</h1>
 }
