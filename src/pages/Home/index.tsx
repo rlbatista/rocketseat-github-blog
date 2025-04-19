043c15b37931e73017d74c549a237795f344a267
Renato L Batista <sonomel00@yahoo.com.br>
2025-04-19 08:43:36 -0300

    adiciona card de perfil

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,39 @@
-import { HomeContainer } from "./styles";
+import { HeaderCard } from '../../components/HeaderCard'
+import logoSvg from '../../assets/logo.svg'
+import { Avatar, DescriptionText, HeaderContainer, HeaderText, HomeContainer, SocialFooter, TextContainer } from './styles'
+import { IconBuilding, IconGithub, IconUpRight, IconUserGroup } from '../../components/Icons'
+import { Link } from 'react-router-dom'
 
 export function Home() {
     return (
-        <HomeContainer>
-            Home
-        </HomeContainer>
+        <>
+            <HeaderCard>
+                <HomeContainer>
+                    <Avatar src={logoSvg} alt="" />
+                    
+                    <TextContainer>
+                        <HeaderContainer>
+                            <HeaderText>Cameron Williamson</HeaderText> 
+                            <Link to="/">
+                                github
+                                <span>
+                                    <IconUpRight />
+                                </span>
+                            </Link>
+                        </HeaderContainer>
+                        <DescriptionText>
+                            Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.
+                        </DescriptionText>
+                        <div>
+                            <SocialFooter>
+                                <li><IconGithub /> <span>cameronwll</span></li>
+                                <li><IconBuilding /> <span>Rocketseat</span></li>
+                                <li><IconUserGroup /> <span>32 seguidores</span></li>
+                            </SocialFooter>
+                        </div>
+                    </TextContainer>
+                </HomeContainer>
+            </HeaderCard>
+        </>
     )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,82 @@
 import styled from "styled-components";
 
-export const HomeContainer = styled.h1`
-    font: ${props => props.theme.fontComponentsLinkXSb}
+export const HomeContainer = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 2rem;
+`
+export const Avatar = styled.img`
+    max-width: 9.25rem;
+    max-height: 9.25rem;
+    width: 9.25rem;
+    height: 9.25rem;
+    border-radius: 8px;
+`
+
+export const TextContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`
+export const HeaderContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 0.5rem;
+
+    & a {
+        position: relative;
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        text-transform: uppercase;
+        font: ${props => props.theme.fontComponentsLinkXSb};
+        color: ${props => props.theme.colorBlue};
+
+        & span {
+            font-size: 0.5875rem;
+            
+            & svg {
+                color: ${props => props.theme.colorBlue};
+            }
+        }
+
+        &::after {
+            content: '';
+            position: absolute;
+            left: 50%;
+            bottom: 0;
+            width: 0;
+            height: 2px;
+            background-color: ${props => props.theme.colorBlue};
+            transition: width 0.3s ease;
+            transform: translateX(-50%);
+        }
+
+        &:hover::after {
+            width: 100%;
+        }
+    }
+`
+
+export const HeaderText = styled.h1`
+    font: ${props => props.theme.fontTitleLb};
+`
+
+export const DescriptionText = styled.p`
+    margin-bottom: 1.5rem;
+    font: ${props => props.theme.fontTextMr};
+    color: ${props => props.theme.colorBaseText};
+`
+
+export const SocialFooter = styled.ul`
+    display: flex;
+    gap: 1.5rem;
+
+    & li {
+        display: flex;
+        gap: 0.5rem;
+    }
 `
