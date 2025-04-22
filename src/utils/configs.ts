a4c03fe6269c5067d984f84e2e43a5f255e7a63b
Renato L Batista <sonomel00@yahoo.com.br>
2025-04-22 08:25:03 -0300

    adiciona nome do repo no arq de configs

--- src/utils/configs.ts
+++ src/utils/configs.ts
@@ -1,13 +1,19 @@
 class Configs {
     private githubAccount: string
     private githubToken: string
+    private githubRepo: string
 
     constructor() {
         this.githubAccount = import.meta.env.VITE_GITHUB_ACCOUNT || undefined
         this.githubToken = import.meta.env.VITE_GITHUB_TOKEN || undefined
+        this.githubRepo = import.meta.env.VITE_GITHUB_REPO || undefined
 
         if(!this.githubAccount) {
             throw new Error('Environmnet variable githubAccout has not been defined')
+        }
+
+        if(!this.githubRepo) {
+            throw new Error('Environment variable githubRepo has not been defined')
         }
     }
 
@@ -17,6 +23,10 @@ class Configs {
 
     public getGithubToken(): string {
         return this.githubToken
+    }
+
+    public getGithubRepo() {
+        return this.githubRepo
     }
 }
 
